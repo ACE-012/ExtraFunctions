8dccbec2e59afd1ec727af7736598b125ce63b63
Note <sikdarswagato0@gmail.com>
2025-04-13 14:08:26 +0530

    initial

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="ExtraFunctions",
+    version="0.1.13042025",
+    include_package_data=True,
+    python_requires='>=3.8',
+    packages=find_packages(),
+    setup_requires=['setuptools-git-versioning'],
+    install_requires=requirements,
+    author="Swagato Sikdar",
+    author_email="sikdarswagato0@gmail.com",
+    description="Commonly used code snippets",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python",
+        "",
+        "Operating System :: OS Independent",
+    ],
+    version_config={
+       "dirty_template": "{tag}",
+    }
+)
+
