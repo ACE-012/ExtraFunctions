dc631e9767867c2053237d87597b7fab325e542c
Note <sikdarswagato0@gmail.com>
2025-04-13 15:03:08 +0530

    .

--- ExtraFunctions/extrafn.py
+++ ExtraFunctions/extrafn.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt 
 import numpy as np    
 
+
+# snippet copied from https://stackoverflow.com/questions/7941226/how-to-add-line-based-on-slope-and-intercept answered by ak_slick and David Marx 
 def line_with_slope_and_intercept(slope, intercept):
     """Plot a line from slope and intercept"""
     axes = plt.gca()
