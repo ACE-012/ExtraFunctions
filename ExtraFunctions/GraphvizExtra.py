97c18b6af0715bdcc78a6b12a781acc7538802c0
Note <sikdarswagato0@gmail.com>
2025-05-07 10:13:53 +0530

    fixgraphviz

--- ExtraFunctions/GraphvizExtra.py
+++ ExtraFunctions/GraphvizExtra.py
@@ -1,20 +1,20 @@
-from . import install
+from __init__ import install
 try:
     import graphviz
 except ModuleNotFoundError:
     install("graphviz") if input("graphviz is not installed\n install?(y/N)").lower()=='y' else exit(0)
     import graphviz
+
 def visualize_tree(tree,name='decision_tree',view=True,format='png'):
-    tree=visualize_tree(tree)
+    tree=__visualize_tree(tree)
     tree.render(name,view=view,format=format)
 
 def __visualize_tree(tree, graph=None, node_id=0):
-       if graph is None:
+    if graph is None:
          graph = graphviz.Digraph(comment='Decision Tree', graph_attr={'rankdir': 'TB'})
-       if type(tree)==str:
+    if type(tree)==str:
          graph.node(str(node_id), label=f"Class: {tree}")
-       else:
-         #print(tree)
+    else:
          graph.node(str(node_id), label=f"Feature: {tree['Label']}\nThreshold: {tree['val']}\n Gini:{tree['gini']}")
          left_child_id = node_id * 2 + 1
          right_child_id = node_id * 2 + 2
@@ -22,13 +22,13 @@ def __visualize_tree(tree, graph=None, node_id=0):
          graph.edge(str(node_id), str(right_child_id), label='Right')
          __visualize_tree(tree['Left'], graph, left_child_id)
          __visualize_tree(tree['Right'], graph, right_child_id)
-       return graph
+    return graph
 
 if __name__=='__main__':
     tree = {
-       'Label': 'feature1', 'val': 0.5,
+       'Label': 'feature1', 'val': 0.5,'gini':0.5,
        'Left': {
-           'Label': 'feature2', 'val': 0.3,
+            'Label': 'feature2', 'val': 0.3,'gini':0.4,
            'Left': "yes",
            'Right': "no"
        },
